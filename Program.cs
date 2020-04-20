4605aeb25a1a0d826102495a60ad938ea5e97cfd
unknown <aminmirzoev123@gmail.com>
2020-04-20 20:22:47 +0500

    Methods

--- Program.cs
+++ Program.cs
@@ -4,20 +4,19 @@ namespace HW14_18_04
 {
     class Program
     {
-       delegate T Operation<T>(T x, T y);
+     delegate T del<T>(T A, T B);
         static void Main(string[] args)
         {
-            bool sss = true;
-            while (sss)
+            bool work = true;
+            while (work)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                System.Console.WriteLine("Choose your operations(*,/,+,-):");
-                System.Console.WriteLine("1.Multiply(*):");
-                System.Console.WriteLine("2.Divide(/):");
-                System.Console.WriteLine("3.Add(+):");
-                System.Console.WriteLine("4.Minus(-):");
-                System.Console.WriteLine("5.Exit:");
-                Console.ForegroundColor = ConsoleColor.White;
+                System.Console.WriteLine("Выберите операцию(*,/,+,-):");
+                System.Console.WriteLine("1.умножения(*):");
+                System.Console.WriteLine("2.деление(/):");
+                System.Console.WriteLine("3.плюс(+):");
+                System.Console.WriteLine("4.минус(-):");
+                System.Console.WriteLine("5.Выход:");
+                
 
                 string choice = Console.ReadLine();
                 switch (choice)
@@ -25,108 +24,106 @@ namespace HW14_18_04
 
                     case "1":
                         {
-                            System.Console.Write("Enter your first number: ");
-                            double x = Convert.ToDouble(Console.ReadLine());
-                            System.Console.Write("Enter your second number: ");
-                            double y = Convert.ToDouble(Console.ReadLine());
-                            Operation<double> mult = Multiply<double>;
-                            double result = mult.Invoke(x, y);
-                            Console.ForegroundColor = ConsoleColor.Green;
+                            System.Console.Write("Наберите число А: ");
+                            double A= double.Parse(Console.ReadLine());
+                            System.Console.Write("Наберите число В: ");
+                            double B= double.Parse(Console.ReadLine());
+                            del<double> mult = Multiply<double>;
+                            double result = mult(A,B);
                             System.Console.WriteLine("======================");
-                            System.Console.WriteLine($"Result is:{result}");
+                            System.Console.WriteLine($"ответ->{result}");
                             System.Console.WriteLine("======================");
-                            Console.ForegroundColor = ConsoleColor.White;
+                           
+                            
                         };
                         break;
 
                     case "2":
                         {
-                            System.Console.Write("Enter your first number:");
-                            double x = Convert.ToDouble(Console.ReadLine());
-                            System.Console.Write("Enter your second number:");
-                            double y = Convert.ToDouble(Console.ReadLine());
-                            if (x != 0 && y != 0)
+                            System.Console.Write("Наберите число А");
+                            double A= double.Parse(Console.ReadLine());
+                            System.Console.Write("Наберите число В");
+                            double B= double.Parse(Console.ReadLine());
+                            if (A != 0 && A != 0)
                             {
-                                Operation<double> div = Divide<double>;
-                                double result = div.Invoke(x, y);
-                                Console.ForegroundColor = ConsoleColor.Green;
+                                del<double> div = Divide<double>;
+                                double result = div(A,B);
+                                
                                 System.Console.WriteLine("======================");
-                                System.Console.WriteLine($"Result is:{result}");
+                                System.Console.WriteLine($"ответ->{result}");
                                 System.Console.WriteLine("======================");
-                                Console.ForegroundColor = ConsoleColor.White;
+                                
                             }
                             else
                             {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                System.Console.WriteLine("======================");
-                                Console.WriteLine("You are writed wrong number, 0 can't divide any number!!!");
-                                System.Console.WriteLine("======================");
-                                Console.ForegroundColor = ConsoleColor.White;
+                                
+                                System.Console.WriteLine("Error!!!");
+                                
                             }
 
                         }; break;
 
                     case "3":
                         {
-                            System.Console.Write("Enter your first number: ");
-                            double x = Convert.ToDouble(Console.ReadLine());
-                            System.Console.Write("Enter your second number: ");
-                            double y = Convert.ToDouble(Console.ReadLine());
-                            Operation<double> sum = Sum<double>;
-                            double result = sum.Invoke(x, y);
+                            System.Console.Write("Наберите число А ");
+                            double A= double.Parse(Console.ReadLine());
+                            System.Console.Write("Наберите число В ");
+                            double B= double.Parse(Console.ReadLine());
+                            del<double> sum = Sum<double>;
+                            double result = sum.Invoke(A, B);
                             Console.ForegroundColor = ConsoleColor.Green;
                             System.Console.WriteLine("======================");
-                            System.Console.WriteLine($"Result is: {result}");
+                            System.Console.WriteLine($"ответ-> {result}");
                             System.Console.WriteLine("======================");
                             Console.ForegroundColor = ConsoleColor.White;
                         }; break;
 
                     case "4":
                         {
-                            System.Console.Write("Enter your first number: ");
-                            double x = Convert.ToDouble(Console.ReadLine());
-                            System.Console.Write("Enter your second number: ");
-                            double y = Convert.ToDouble(Console.ReadLine());
-                            Operation<double> min = Minus<double>;
-                            double result = min.Invoke(x, y);
+                            System.Console.Write("Наберите число А ");
+                            double A= double.Parse(Console.ReadLine());
+                            System.Console.Write("Наберите число В");
+                            double B= double.Parse(Console.ReadLine());
+                            del<double> min = Minus<double>;
+                            double result = min.Invoke(A, B);
                             Console.ForegroundColor = ConsoleColor.Green;
                             System.Console.WriteLine("======================");
-                            System.Console.WriteLine($"Result is: {result}");
+                            System.Console.WriteLine($"ответ-> {result}");
                             System.Console.WriteLine("======================");
                             Console.ForegroundColor = ConsoleColor.White;
                         }; break;
 
 
-                    case "5": sss = false; break;
+                    case "5": work = false; break;
 
                     default:
-                        Console.ForegroundColor = ConsoleColor.Red;
+                       
                         System.Console.WriteLine("======================");
                         Console.WriteLine("You are writed wrong number!!!");
                         System.Console.WriteLine("======================");
-                        Console.ForegroundColor = ConsoleColor.White;
+                        
                         break;
                 }
             }
         }
-        static T Multiply<T>(T x, T y)
+        static T Multiply<T>(T A, T B)
         {
 
-            return (dynamic)x * (dynamic)y;
+            return (dynamic)A * (dynamic)A;
         }
-        static T Divide<T>(T x, T y)
+        static T Divide<T>(T A, T B)
         {
 
-            return (dynamic)x / (dynamic)y;
+            return (dynamic)A / (dynamic)B;
         }
-        static T Sum<T>(T x, T y)
+        static T Sum<T>(T A, T B)
         {
-            return (dynamic)x + (dynamic)y;
+            return (dynamic)A + (dynamic)B;
         }
 
-        static T Minus<T>(T x, T y)
+        static T Minus<T>(T A, T B)
         {
-            return (dynamic)x - (dynamic)y;
+            return (dynamic)A - (dynamic)B;
         }
     }
 
